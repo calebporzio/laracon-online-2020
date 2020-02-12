4f5266534e7040060dc24d721c5fce244304c809
Caleb Porzio <calebporzio@gmail.com>
2020-02-12 15:47:39 -0500

    wip

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,11 +2,13 @@
 <head>
     <link rel="stylesheet" href="/app.css">
     @livewireStyles
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v1.10.1/dist/alpine.js" defer></script>
 </head>
 <body>
     @yield('content')
 
     @livewireScripts
+    <script src="https://cdn.jsdelivr.net/gh/livewire/alpine-plugin@v0.1.0/dist/livewire-alpine-plugin.js"></script>
     @stack('scripts')
 </body>
 </html>
