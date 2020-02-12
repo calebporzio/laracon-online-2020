49664fcafdfaa625744bf6c41396c395acda6540
Caleb Porzio <calebporzio@gmail.com>
2020-02-12 15:17:47 -0500

    wip

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,10 +1,12 @@
 <html>
 <head>
     <link rel="stylesheet" href="/app.css">
+    @livewireStyles
 </head>
 <body>
     @yield('content')
 
+    @livewireScripts
     @stack('scripts')
 </body>
 </html>
