8a92479e22a3d84dd2e4ed563c2e3689629f0393
Caleb Porzio <calebporzio@gmail.com>
2020-02-12 21:03:55 -0500

    wip

---
+++ app/Http/Livewire/Tests.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Tests extends Component
+{
+    public function render()
+    {
+        return view('livewire.tests');
+    }
+}
