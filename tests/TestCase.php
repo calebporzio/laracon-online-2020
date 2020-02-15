16ddd314e6799ab74be0c0137df9ce66b84e3749
Caleb Porzio <calebporzio@gmail.com>
2020-02-15 14:57:56 -0500

    wip

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,24 @@
 
 namespace Tests;
 
+use App\Console\Kernel;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Schema;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
+    public function createApplication()
+    {
+        $app = require __DIR__.'/../bootstrap/app.php';
+
+        $app->make(Kernel::class)->bootstrap();
+
+        // Refresh sushi.
+        Schema::refreshDatabaseFile();
+        \App\Repo::setSqliteConnection(__DIR__.'/../database/database.sqlite');
+        (new \App\Repo)->migrate();
+
+        return $app;
+    }
 }
